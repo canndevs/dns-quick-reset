a9da5f3aaec49e1b41c17d366b8150f7029114e7
Atacan Cengiz <68880378+canndevs@users.noreply.github.com>
2022-03-19 19:59:10 +0300

    Add files via upload

---
+++ reset.cmd
@@ -0,0 +1,10 @@
+@echo off
+color a
+ipconfig /flushdns
+ipconfig /release
+ipconfig /renew
+cls
+echo DNS Resetlemesi yapildi
+echo Cikis icin herhangibir tusa basin.
+echo daha fazla icin atacancengiz.com.tr
+pause
